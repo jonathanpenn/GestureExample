a14f9c911ef4e55f29cf5b9aca861c50edfda86d
Jonathan Penn <jonathan@navel-labs.com>
2012-06-11 21:03:18 -0700

    Initial Commit

---
+++ GestureExample/CMNAppDelegate.m
@@ -0,0 +1,16 @@
+#import "CMNAppDelegate.h"
+
+@implementation CMNAppDelegate
+
+@synthesize window = _window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end

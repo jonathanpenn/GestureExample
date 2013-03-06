bedf43b9ade8f2f08ba15b755fff14c2adc99efb
Jonathan Penn <jonathan@navel-labs.com>
2013-03-06 06:47:47 -0500

    Add explaination for subclass

--- GestureExample/TrackingGestureRecognizer.h
+++ GestureExample/TrackingGestureRecognizer.h
@@ -1,5 +1,9 @@
 #import <UIKit/UIKit.h>
 
+// This subclass is special because it transitions to the "Began" state
+// immediately on the first touch. The normal pan gesture transitions on
+// first touch move, which doesn't work for the tracking behavior we want.
+
 @interface TrackingGestureRecognizer : UIPanGestureRecognizer
 
 @end
